f225f702b212e071a5a2da1d7314ef6077171eb0
the-bug <jonas.scholz@die-honigwanderer.de>
2018-03-20 08:43:47 +0000

    refactored honigraumwaage

--- honigraumwaage.py
+++ honigraumwaage.py
@@ -1,59 +1,47 @@
 ﻿import hal.scale as scale
 import hal.keypad as keypad
-import time
 from sending import Sending
 import logging
+import time
 
-    
-hiveMark = ""
-restart = False
-def main():
-    logging.basicConfig(filename='Honigraumwaage.log',level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s' )    
-    
-    global hiveMark
-    global restart
-    
-    restart = False
-    hiveMark = ""
-    
-    print("Starte")
-    
-    sending = Sending()
-    
-    with scale.Scale() as myScale:
-        def print_key(key):
-            global hiveMark
-            global restart
-            if key == 'D':
-                weight = myScale.getWeight()
-                print('Sende Gewicht %d mit Nummer %s' %(weight, hiveMark))                
-                sendingStatus = sending.sendWeight(weight, hiveMark)
-                if sendingStatus:
-                    print "Senden OK"
+class Honigraumwaage:
+    def __init__(self):
+        print("Starte")
+        self.hiveMark = ""
+        self.restart = False
+        logging.basicConfig(filename='Honigraumwaage.log',level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')   
+        self.sending = Sending()
+        self.run()
+
+    def run(self):
+        self.restart = False
+        with scale.Scale() as myScale:
+            def print_key(key):
+                if key == 'D':
+                    weight = myScale.getWeight()
+                    print('Sende Gewicht %d mit Nummer %s' %(weight, self.hiveMark))                
+                    sendingStatus = self.sending.sendWeight(weight, self.hiveMark)
+                    if sendingStatus:
+                        print "Senden OK"
+                    else:
+                        print "Beim Senden ist etwas schiefgegangen"                        
+                    self.hiveMark = ""                
+                elif key == 'C':
+                    self.hiveMark = ""   
+                    print("Setzte Nummer zurück")                                
+                elif key == 'A':
+                    print("Starte Anwendung neu")
+                    self.hiveMark = ""
+                    myScale.reset()
                 else:
-                    print "Beim Senden ist etwas schiefgegangen"                        
-                hiveMark = ""                
-            elif key == 'C':
-                hiveMark = ""   
-                print("Setzte Nummer zurück")                                
-            elif key == 'A':
-                print("Starte Anwendung neu")
-                hiveMark = ""
-                restart = True
-            else:
-                hiveMark += key
-                print(hiveMark)
-                
-        with keypad.Keypad(print_key) as myKeypad:
-            while not restart:
-                val = myScale.getWeight()
-                print "Gewicht: %d" %(val)
-                time.sleep(1)
-
-    if restart:
-        print("restart")
-        main()
-    
+                    self.hiveMark += key
+                    print(self.hiveMark)
+                    
+            with keypad.Keypad(print_key) as myKeypad:
+                while True:
+                    val = myScale.getWeight()
+                    print "Gewicht: %d" %(val)
+                    time.sleep(1)
 
 if __name__ == "__main__":
-    main()
+    honigraumwaage = Honigraumwaage()    

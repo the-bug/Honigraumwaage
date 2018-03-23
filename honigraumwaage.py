2cebbffdcdf56644c03d190c9abac8aa91259bb0
the-bug <jonas.scholz@die-honigwanderer.de>
2018-03-23 07:10:37 +0000

    Added raspberry Pi shutdown as keypad command

--- honigraumwaage.py
+++ honigraumwaage.py
@@ -9,22 +9,28 @@ class Honigraumwaage:
         print("Starte")
         self.hiveMark = ""
         self.restart = False
+        self.shutdown = False
         logging.basicConfig(filename='Honigraumwaage.log',level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')   
         self.sending = Sending()
         self.run()
 
     def run(self):
         self.restart = False
+        self.shutdown = False
         with scale.Scale() as myScale:
             def print_key(key):
                 if key == 'D':
-                    weight = myScale.getWeight()
-                    print('Sende Gewicht %d mit Nummer %s' %(weight, self.hiveMark))                
-                    sendingStatus = self.sending.sendWeight(weight, self.hiveMark)
-                    if sendingStatus:
-                        print "Senden OK"
+                    if self.hiveMark == "*0#":
+                        print "Fahre herrunter"    
+                        self.shutdown = True
                     else:
-                        print "Beim Senden ist etwas schiefgegangen"                        
+                        weight = myScale.getWeight()
+                        print('Sende Gewicht %d mit Nummer %s' %(weight, self.hiveMark))                
+                        sendingStatus = self.sending.sendWeight(weight, self.hiveMark)
+                        if sendingStatus:
+                            print "Senden OK"
+                        else:
+                            print "Beim Senden ist etwas schiefgegangen"                        
                     self.hiveMark = ""                
                 elif key == 'C':
                     self.hiveMark = ""   
@@ -38,10 +44,18 @@ class Honigraumwaage:
                     print(self.hiveMark)
                     
             with keypad.Keypad(print_key) as myKeypad:
-                while True:
+                while not self.shutdown:
                     val = myScale.getWeight()
                     print "Gewicht: %d" %(val)
                     time.sleep(1)
+                    
+        if self.shutdown:
+            self._shutdown()
 
+    def _shutdown(self):
+        from subprocess import call
+        call("sudo shutdown -h now", shell=True)
+        
+        
 if __name__ == "__main__":
     honigraumwaage = Honigraumwaage()    
